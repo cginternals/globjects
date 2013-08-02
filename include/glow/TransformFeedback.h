29dc186e8e7f4b3cebf3fc76076a308e6976b1c1
Willy Scheibel <willyscheibel@gmx.de>
2013-08-02 15:31:12 +0200

    add documentation for TransformFeedback (ref #20)

--- include/glow/TransformFeedback.h
+++ include/glow/TransformFeedback.h
@@ -9,6 +9,42 @@
 namespace glow
 {
 
+/** \brief The Transform Feedback class provides the interface to use transform feedback.
+ * 
+ * The TransformFeedback methods map directly to their OpenGL function counterparts.
+ * The draw and setVaryings method additionally binds the TransformFeedback object,
+ * whereas the begin, pause, resume and end methods doesn't.
+ *
+ * An example usage:
+ * \code{.cpp}
+ * Program* program = createProgramIncludingShaders();
+ * TransformFeedback* transformFeeback = new TransformFeedback();
+ * Buffer* buffer = new glow::Buffer(GL_ARRAY_BUFFER);
+ * 
+ * transformFeeback->setVaryings(program, Array<const char*>{ "vertex_out" }, GL_INTERLEAVED_ATTRIBS);
+ * 
+ * buffer->bindBase(GL_TRANSFORM_FEEDBACK_BUFFER, 0);
+ * buffer->setData(vertexSize, nullptr, GL_DYNAMIC_DRAW);
+ * 
+ * program->use(); // use Program before issueing the begin method of TransformFeedback
+ * 
+ * transformFeeback->bind();
+ * transformFeeback->begin(GL_POINTS); // record point vertices
+ * 
+ * // draw calls
+ * 
+ * transformFeeback->end(); // stop recording vertices before releasing the Program
+ * transformFeeback->unbind();
+ * 
+ * program->release();
+ * 
+ * // replay recorded vertices
+ * transformFeeback->draw(GL_POINTS);
+ * \endcode
+ * 
+ * \see http://www.opengl.org/registry/specs/ARB/transform_feedback2.txt
+ * \see http://www.opengl.org/registry/specs/ARB/transform_feedback3.txt
+ */
 class GLOW_API TransformFeedback : public Object
 {
 public:
