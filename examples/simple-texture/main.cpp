63cb6375d8fc1b82e6be310c3cb88eafc8ddfbac
Willy Scheibel <willyscheibel@gmx.de>
2013-11-08 11:00:01 +0100

    Rewrote simple texture  example to use screen aligned quad

--- examples/simple-texture/main.cpp
+++ examples/simple-texture/main.cpp
@@ -1,25 +1,13 @@
-
 #include <GL/glew.h>
 
 #include <algorithm>
 #include <random>
 
-#include <glm/glm.hpp>
-#include <glm/gtc/type_ptr.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtx/transform.hpp>
-
 #include <glow/Error.h>
-#include <glow/Uniform.h>
 #include <glow/Array.h>
-#include <glow/ShaderFile.h>
-#include <glow/Program.h>
-#include <glow/Shader.h>
-#include <glow/Buffer.h>
-#include <glow/VertexArrayObject.h>
-#include <glow/VertexAttributeBinding.h>
 #include <glow/logging.h>
-
+#include <glow/Texture.h>
+#include <glowutils/ScreenAlignedQuad.h>
 #include <glowwindow/ContextFormat.h>
 #include <glowwindow/Context.h>
 #include <glowwindow/Window.h>
@@ -39,7 +27,6 @@ public:
     }
 
     void createAndSetupTexture();
-    void createAndSetupShaders();
 	void createAndSetupGeometry();
 
     virtual void initialize(Window & window)
@@ -48,8 +35,7 @@ public:
 
         glClearColor(0.2f, 0.3f, 0.4f, 1.f);
 
-	    createAndSetupTexture();
-	    createAndSetupShaders();
+        createAndSetupTexture();
 	    createAndSetupGeometry();
     }
     
@@ -59,23 +45,14 @@ public:
     ,   const unsigned int height)
     {
     	int side = std::min<int>(width, height);
-	    glViewport((width - side) / 2, (height - side) / 2, side, side);
-
-	    m_shaderProgram->setUniform("modelView", glm::mat4());
-	    m_shaderProgram->setUniform("projection", glm::ortho(0.f, 1.f, 0.f, 1.f, 0.f, 1.f));
+        glViewport((width - side) / 2, (height - side) / 2, side, side);
     }
 
     virtual void paintEvent(Window & window)
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	    m_texture->bind();
-	    m_shaderProgram->use();
-
-        m_vertexArrayObject->drawArrays(GL_TRIANGLE_FAN, 0, 4);
-
-        m_shaderProgram->release();
-        m_texture->unbind();
+        m_quad->draw();
     }
 
     virtual void idleEvent(Window & window)
@@ -85,11 +62,7 @@ public:
 
 protected:
     glow::ref_ptr<glow::Texture> m_texture;
-
-    glow::ref_ptr<glow::Program> m_shaderProgram;
-	glow::ref_ptr<glow::VertexArrayObject> m_vertexArrayObject;
-	
-    glow::ref_ptr<glow::Buffer> m_vertexBuffer;
+    glow::ref_ptr<glow::ScreenAlignedQuad> m_quad;
 };
 
 
@@ -136,37 +109,8 @@ void EventHandler::createAndSetupTexture()
 	m_texture->image2D(0, GL_RGBA8, w, h, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
 }
 
-void EventHandler::createAndSetupShaders()
-{
-	glow::Shader * vertexShader = glow::Shader::fromFile(GL_VERTEX_SHADER, "data/simple-texture/test.vert");
-    glow::Shader * fragmentShader = glow::Shader::fromFile(GL_FRAGMENT_SHADER, "data/simple-texture/test.frag");
-
-	m_shaderProgram = new glow::Program();
-	m_shaderProgram->attach(vertexShader, fragmentShader);
-	m_shaderProgram->bindFragDataLocation(0, "fragColor");
-
-	m_shaderProgram->getUniform<GLint>("texture")->set(0);
-}
-
 void EventHandler::createAndSetupGeometry()
 {
-	auto vertices = glow::Array<glm::vec3>()
-		<< glm::vec3(0,0,0)
-		<< glm::vec3(1,0,0)
-		<< glm::vec3(1,1,0)
-		<< glm::vec3(0,1,0);
-
-	m_vertexArrayObject = new glow::VertexArrayObject();
-
-	m_vertexBuffer = new glow::Buffer(GL_ARRAY_BUFFER);
-	m_vertexBuffer->setData(vertices);
-
-	auto binding1 = m_vertexArrayObject->binding(0);
-    auto a_vertex = m_shaderProgram->getAttributeLocation("a_vertex");
-
-	binding1->setAttribute(a_vertex);
-	binding1->setBuffer(m_vertexBuffer, 0, sizeof(glm::vec3));
-	binding1->setFormat(3, GL_FLOAT, GL_FALSE, 0);
-
-	m_vertexArrayObject->enable(a_vertex);
+    m_quad = new glow::ScreenAlignedQuad(m_texture);
+    m_quad->setSamplerUniform(0);
 }
