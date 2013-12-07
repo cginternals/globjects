974018a4dc13d30e1c51727e766f8669c5f372dc
Stefan Buschmann <stefan.buschmann@hpi.uni-potsdam.de>
2013-12-07 23:04:16 +0100

    fix compilation with newer versions of GLM (seems compatible with old version)

--- source/glowutils/AxonometricLookAt.cpp
+++ source/glowutils/AxonometricLookAt.cpp
@@ -169,10 +169,10 @@ void AxonometricLookAt::rebuild() const
     const mat4 vrot = mat4_cast(angleAxis(-m_verticalAngle, vec3( 0.f, 1.f, 0.f)));
     const mat4 hrot = mat4_cast(angleAxis(m_horizontalAngle, vec3( 1.f, 0.f, 0.f)));
 
-    const mat4 zoom = scale(m_zoom, m_zoom, 1.f);
+    const mat4 zoom = scale(vec3(m_zoom, m_zoom, 1.f));
     const mat4 t    = translate(-m_position);
 
-    const mat4 T1 = translate(0.f, 0.f, -512.f);
+    const mat4 T1 = translate(vec3(0.f, 0.f, -512.f));
 
     m_axonometric = T1 * hrot * vrot * t; // * m_rotation;
     m_rebuild = false;
