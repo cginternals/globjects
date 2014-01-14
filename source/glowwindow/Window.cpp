849f7dcff4d7e3e85647fa07d25436b5aa771575
Sebastian Kay Belle <sebastian.belle@hpi.uni-potsdam.de>
2014-01-14 15:06:09 +0100

    Test of fix for issue 118.

--- source/glowwindow/Window.cpp
+++ source/glowwindow/Window.cpp
@@ -149,8 +149,15 @@ void Window::promoteContext(int width, int height)
          m_eventHandler->initialize(*this);
          m_context->doneCurrent();
 
+      int w;
+      int h;
+      
+//      glfwSetWindowSize(m_window, width, height);
+      glfwGetFramebufferSize(m_window, &w, &h);
+      
          queueEvent(new ResizeEvent(glm::ivec2(width, height)));
-         queueEvent(new ResizeEvent(glm::ivec2(width, height), true));
+         queueEvent(new ResizeEvent(glm::ivec2(w, h), true));
+      
     }
 }
 
