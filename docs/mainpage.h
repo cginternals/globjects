764f6ea862254a604a7be75b34a7fb4715656e1f
Roland Lux <rollux2000@gmail.com>
2014-01-29 15:30:31 +0100

    Add a little content to the mainpage

--- docs/mainpage.h
+++ docs/mainpage.h
@@ -1,5 +1,7 @@
 /**
- * \mainpage 
+ * \mainpage
  *
- * TODO: Main page content
+ * The OpenGL Objects Wrapper Library (glow) provides an object oriented C++ interface for
+ *  OpenGL's programmable pipeline (3.0+). 
+ * glow is a cross-platform library licenced under the MIT license.
  */
