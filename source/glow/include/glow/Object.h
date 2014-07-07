9a36e3d42c6d18fd56a6c209b16d10089ac3699b
Roland Lux <rollux2000@gmail.com>
2014-07-07 14:25:21 +0200

    Fix documentation of Object

--- source/glow/include/glow/Object.h
+++ source/glow/include/glow/Object.h
@@ -16,8 +16,7 @@ class IDResource;
 /** \brief Superclass of all wrapped OpenGL objects.
     
     The superclass is Referenced so that each wrapped OpenGL object supports reference counting.
-    Subclasses should call the Object constructor passing a valid OpenGL object name (id) and a flag whether this OpenGL object should be destroyed during the destructor.
-    The OpenGL name (id) that was provided in the constructor can be queried using id().
+    The OpenGL name (id) of the OpenGL resource that was provided in the constructor can be queried using id().
     Additionally, an Object can have meaningful name wich can be get and set using name() and setName().
  */
 class GLOW_API Object : public Referenced
