1f6e5867ee83294aebcd94051983e82d4ee47083
mrzzzrm <mrzesn@t-online.de>
2014-11-22 00:39:47 +0000

    Add forward declaration header, fix #265

---
+++ source/globjects/include/globjects/fwd.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "base/ref_ptr.h"
+
+
+namespace globjects
+{
+    class AbstractUniform;
+    class AbstractState;
+    class AttachedRenderbuffer;
+    class AttachedTexture;
+    class Buffer;
+    class Capability;
+    class DebugInfo;
+    class Error;
+    class Framebuffer;
+    class FramebufferAttachment;
+    class LocationIdentity;
+    class NamedString;
+    class Object;
+    class ObjectVisitor;
+    class Program;
+    class ProgramBinary;
+    class Query;
+    class Renderbuffer;
+    class Sampler;
+    class Shader;
+    class State;
+    class StateSetting;
+    class StateSettingType;
+    class Sync;
+    class Texture;
+    class TransformFeedback;
+    template<typename> class Uniform;
+    class UniformBlock;
+    class VertexArray;
+    class VertexAttributeBinding;
+}
+
