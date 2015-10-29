5a254b3646e1976102bc71fc76cfa855df7befca
Willy Scheibel <willy.scheibel@hpi.de>
2015-10-29 21:37:52 +0100

    Heavier use of compile-time operations

--- source/globjects/include/globjects/TransformFeedback.hpp
+++ source/globjects/include/globjects/TransformFeedback.hpp
@@ -7,12 +7,11 @@ namespace globjects {
 template <std::size_t Count>
 void TransformFeedback::setVaryings(const Program * program, const std::array<std::string, Count> & varyingNames, gl::GLenum bufferMode)
 {
-    std::vector<const char*> c_ptrs;
-    c_ptrs.reserve(varyingNames.size());
+    std::array<const char*, Count> c_ptrs;
 
-    for (auto & name : varyingNames)
+    for (size_t i = 0; i < Count; ++i)
     {
-        c_ptrs.push_back(name.c_str());
+        c_ptrs[i] = varyingNames[i].c_str();
     }
 
     setVaryings(program, static_cast<gl::GLint>(Count), const_cast<const char**>(c_ptrs.data()), bufferMode);
