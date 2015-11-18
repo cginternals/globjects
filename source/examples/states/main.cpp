5c7d9dded362b02c633cc71371b99f6ce6d7ebbd
Leander Neiß <leander.neiss@student.hpi.uni-potsdam.de>
2015-11-18 15:45:15 +0100

    Rewrite states example to not use the common module anymore. #291

--- source/examples/states/main.cpp
+++ source/examples/states/main.cpp
@@ -1,5 +1,12 @@
 
+#include <glm/glm.hpp>
+
 #include <glbinding/gl/gl.h>
+#include <glbinding/ContextInfo.h>
+#include <glbinding/Version.h>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
 
 #include <globjects/globjects.h>
 #include <globjects/logging.h>
@@ -11,58 +18,124 @@
 #include <globjects/VertexAttributeBinding.h>
 #include <globjects/State.h>
 
-#include <common/Context.h>
-#include <common/ContextFormat.h>
-#include <common/Window.h>
-#include <common/WindowEventHandler.h>
-#include <common/events.h>
-
 
 using namespace gl;
 using namespace glm;
 using namespace globjects;
 
-class EventHandler : public WindowEventHandler
+
+void key_callback(GLFWwindow * window, int key, int /*scancode*/, int action, int /*modes*/)
 {
-public:
-    EventHandler()
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void draw(Program * shaderProgram, VertexArray * vao, State * thinnestPointSizeState, State * thinPointSizeState, State * normalPointSizeState, State * thickPointSizeState, State * disableRasterizerState, State * enableRasterizerState, State * defaultPointSizeState)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    shaderProgram->use();
+
+    defaultPointSizeState->apply();
+    vao->drawArrays(GL_POINTS, 0, 5);
+
+    thinnestPointSizeState->apply();
+    vao->drawArrays(GL_POINTS, 5, 5);
+
+    thinPointSizeState->apply();
+    vao->drawArrays(GL_POINTS, 10, 5);
+
+    normalPointSizeState->apply();
+    vao->drawArrays(GL_POINTS, 15, 5);
+
+    thickPointSizeState->apply();
+
+    vao->drawArrays(GL_POINTS, 20, 1);
+    disableRasterizerState->apply();
+    vao->drawArrays(GL_POINTS, 21, 1);
+    enableRasterizerState->apply();
+    vao->drawArrays(GL_POINTS, 22, 1);
+    disableRasterizerState->apply();
+    vao->drawArrays(GL_POINTS, 23, 1);
+    enableRasterizerState->apply();
+    vao->drawArrays(GL_POINTS, 24, 1);
+
+    normalPointSizeState->apply();
+    vao->drawArrays(GL_POINTS, 25, 5);
+
+    thinPointSizeState->apply();
+    vao->drawArrays(GL_POINTS, 30, 5);
+
+    thinnestPointSizeState->apply();
+    vao->drawArrays(GL_POINTS, 35, 5);
+
+    defaultPointSizeState->apply();
+    vao->drawArrays(GL_POINTS, 35, 5);
+
+    shaderProgram->release();
+}
+
+
+int main(int /*argc*/, char * /*argv*/[])
+{
+    // Initialize GLFW with error callback and needed OpenGL version window hint
+    glfwInit();
+    glfwSetErrorCallback( [] (int /*error*/, const char * description) { puts(description); } );
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+
+    // Create a context and, if valid, make it current
+    GLFWwindow * window = glfwCreateWindow(1024, 768, "", NULL, NULL);
+    if (window == nullptr)
     {
+        critical() << "Context creation failed. Terminate execution.";
+
+        glfwTerminate();
+        return 1;
     }
+    glfwMakeContextCurrent(window);
 
-    virtual ~EventHandler()
+    // Create callback that when user presses ESC, the context should be destroyed and window closed
+    glfwSetKeyCallback(window, key_callback);
+
+    // Initialize globjects (internally initializes glbinding, and registers the current context)
+    globjects::init();
+
+    // Dump information about context and graphics card
+    info() << std::endl
+        << "OpenGL Version:  " << glbinding::ContextInfo::version() << std::endl
+        << "OpenGL Vendor:   " << glbinding::ContextInfo::vendor() << std::endl
+        << "OpenGL Renderer: " << glbinding::ContextInfo::renderer() << std::endl;
+    
+
+    glClearColor(0.2f, 0.3f, 0.4f, 1.f);
+
     {
-    }
+        // Initialize
+        ref_ptr<State> defaultPointSizeState  = new State();
+        defaultPointSizeState->pointSize(getFloat(GL_POINT_SIZE));
+        ref_ptr<State> thinnestPointSizeState = new State();
+        thinnestPointSizeState->pointSize(2.0f);
+        ref_ptr<State> thinPointSizeState     = new State();
+        thinPointSizeState->pointSize(5.0f);
+        ref_ptr<State> normalPointSizeState   = new State();
+        normalPointSizeState->pointSize(10.0f);
+        ref_ptr<State> thickPointSizeState    = new State();
+        thickPointSizeState->pointSize(20.0f);
+        ref_ptr<State> disableRasterizerState = new State();
+        disableRasterizerState->enable(GL_RASTERIZER_DISCARD);
+        ref_ptr<State> enableRasterizerState  = new State();
+        enableRasterizerState->disable(GL_RASTERIZER_DISCARD);
 
-    virtual void initialize(Window & window) override
-    {
-        WindowEventHandler::initialize(window);
+        ref_ptr<VertexArray> vao = new VertexArray();
+        ref_ptr<Buffer> buffer = new Buffer();
 
-        glClearColor(0.2f, 0.3f, 0.4f, 1.f);
-
-        m_defaultPointSizeState  = new State();
-        m_defaultPointSizeState->pointSize(getFloat(GL_POINT_SIZE));
-        m_thinnestPointSizeState = new State();
-        m_thinnestPointSizeState->pointSize(2.0f);
-        m_thinPointSizeState     = new State();
-        m_thinPointSizeState->pointSize(5.0f);
-        m_normalPointSizeState   = new State();
-        m_normalPointSizeState->pointSize(10.0f);
-        m_thickPointSizeState    = new State();
-        m_thickPointSizeState->pointSize(20.0f);
-        m_disableRasterizerState = new State();
-        m_disableRasterizerState->enable(GL_RASTERIZER_DISCARD);
-        m_enableRasterizerState  = new State();
-        m_enableRasterizerState->disable(GL_RASTERIZER_DISCARD);
-
-        m_vao = new VertexArray();
-        m_buffer = new Buffer();
-
-        m_shaderProgram = new Program();
-        m_shaderProgram->attach(
+        ref_ptr<Program> shaderProgram = new Program();
+        shaderProgram->attach(
             Shader::fromFile(GL_VERTEX_SHADER, "data/states/standard.vert")
           , Shader::fromFile(GL_FRAGMENT_SHADER, "data/states/standard.frag"));
         
-        m_buffer->setData(std::vector<vec2>({
+        buffer->setData(std::vector<vec2>({
             vec2(-0.8f, 0.8f), vec2(-0.4f, 0.8f), vec2( 0.0f, 0.8f), vec2( 0.4f, 0.8f), vec2( 0.8f, 0.8f)
           , vec2(-0.8f, 0.6f), vec2(-0.4f, 0.6f), vec2( 0.0f, 0.6f), vec2( 0.4f, 0.6f), vec2( 0.8f, 0.6f)
           , vec2(-0.8f, 0.4f), vec2(-0.4f, 0.4f), vec2( 0.0f, 0.4f), vec2( 0.4f, 0.4f), vec2( 0.8f, 0.4f)
@@ -74,95 +147,24 @@ public:
           , vec2(-0.8f,-0.8f), vec2(-0.4f,-0.8f), vec2( 0.0f,-0.8f), vec2( 0.4f,-0.8f), vec2( 0.8f,-0.8f) })
           , GL_STATIC_DRAW );
 
-        m_vao->binding(0)->setAttribute(0);
-        m_vao->binding(0)->setBuffer(m_buffer, 0, sizeof(vec2));
-        m_vao->binding(0)->setFormat(2, GL_FLOAT);
-        m_vao->enable(0);
+        vao->binding(0)->setAttribute(0);
+        vao->binding(0)->setBuffer(buffer, 0, sizeof(vec2));
+        vao->binding(0)->setFormat(2, GL_FLOAT);
+        vao->enable(0);
+
+
+        // Main loop
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+            draw(shaderProgram, vao, thinnestPointSizeState, thinPointSizeState, normalPointSizeState, thickPointSizeState, disableRasterizerState, enableRasterizerState, defaultPointSizeState);
+            glfwSwapBuffers(window);
+        }
+
     }
 
-    virtual void paintEvent(PaintEvent & event) override
-    {
-        WindowEventHandler::paintEvent(event);
+    // Properly shutdown GLFW
+    glfwTerminate();
 
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        m_shaderProgram->use();
-
-        m_defaultPointSizeState->apply();
-        m_vao->drawArrays(GL_POINTS, 0, 5);
-
-        m_thinnestPointSizeState->apply();
-        m_vao->drawArrays(GL_POINTS, 5, 5);
-
-        m_thinPointSizeState->apply();
-        m_vao->drawArrays(GL_POINTS, 10, 5);
-
-        m_normalPointSizeState->apply();
-        m_vao->drawArrays(GL_POINTS, 15, 5);
-
-        m_thickPointSizeState->apply();
-
-        m_vao->drawArrays(GL_POINTS, 20, 1);
-        m_disableRasterizerState->apply();
-        m_vao->drawArrays(GL_POINTS, 21, 1);
-        m_enableRasterizerState->apply();
-        m_vao->drawArrays(GL_POINTS, 22, 1);
-        m_disableRasterizerState->apply();
-        m_vao->drawArrays(GL_POINTS, 23, 1);
-        m_enableRasterizerState->apply();
-        m_vao->drawArrays(GL_POINTS, 24, 1);
-
-        m_normalPointSizeState->apply();
-        m_vao->drawArrays(GL_POINTS, 25, 5);
-
-        m_thinPointSizeState->apply();
-        m_vao->drawArrays(GL_POINTS, 30, 5);
-
-        m_thinnestPointSizeState->apply();
-        m_vao->drawArrays(GL_POINTS, 35, 5);
-
-        m_defaultPointSizeState->apply();
-        m_vao->drawArrays(GL_POINTS, 35, 5);
-
-        m_shaderProgram->release();
-    }
-
-protected:
-    ref_ptr<VertexArray> m_vao;
-    ref_ptr<Buffer> m_buffer;
-    ref_ptr<Program> m_shaderProgram;
-
-    ref_ptr<State> m_defaultPointSizeState;
-    ref_ptr<State> m_thinnestPointSizeState;
-    ref_ptr<State> m_thinPointSizeState;
-    ref_ptr<State> m_normalPointSizeState;
-    ref_ptr<State> m_thickPointSizeState;
-    ref_ptr<State> m_disableRasterizerState;
-    ref_ptr<State> m_enableRasterizerState;
-};
-
-
-int main(int /*argc*/, char * /*argv*/[])
-{
-    info() << "Usage:";
-    info() << "\t" << "ESC" << "\t\t"       << "Close example";
-    info() << "\t" << "ALT + Enter" << "\t" << "Toggle fullscreen";
-    info() << "\t" << "F11" << "\t\t"       << "Toggle fullscreen";
-    info() << "\t" << "F10" << "\t\t"       << "Toggle vertical sync";
-
-    ContextFormat format;
-    format.setVersion(3, 1);
-    format.setForwardCompatible(true);
-
-    Window::init();
-
-    Window window;
-    window.setEventHandler(new EventHandler());
-
-    if (!window.create(format, "OpenGL States Example"))
-        return 1;
-
-    window.show();
-
-    return MainLoop::run();
+    return 0;
 }
