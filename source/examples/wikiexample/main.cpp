ecf8547a4a708bf24ecaad60cf6cc871b36ff614
Leander Neiß <leander.neiss@student.hpi.uni-potsdam.de>
2015-11-27 16:09:20 +0100

    Rewrite wikiexample to not use the common module anymore. #291

--- source/examples/wikiexample/main.cpp
+++ source/examples/wikiexample/main.cpp
@@ -1,5 +1,15 @@
 
+#include <glm/glm.hpp>
+
 #include <glbinding/gl/gl.h>
+#include <glbinding/ContextInfo.h>
+#include <glbinding/Version.h>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include <globjects/globjects.h>
+#include <globjects/logging.h>
 
 #include <globjects/Buffer.h>
 #include <globjects/Program.h>
@@ -7,12 +17,6 @@
 #include <globjects/VertexArray.h>
 #include <globjects/VertexAttributeBinding.h>
 #include <globjects/base/StaticStringSource.h>
-
-#include <common/Window.h>
-#include <common/ContextFormat.h>
-#include <common/Context.h>
-#include <common/WindowEventHandler.h>
-#include <common/events.h>
 
 
 using namespace gl;
@@ -52,82 +56,87 @@ void main()
 )";
 }
 
-class EventHandler : public WindowEventHandler
+
+void key_callback(GLFWwindow * window, int key, int /*scancode*/, int action, int /*modes*/)
 {
-public:
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
+        glfwSetWindowShouldClose(window, true);
+}
 
-    EventHandler()
-    : m_vao(nullptr)
-    , m_cornerBuffer(nullptr)
-    , m_program(nullptr)
-    {
-    }
+void draw(Program * program, VertexArray * vao)
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    virtual ~EventHandler()
-    {
-    }
-
-    virtual void initialize(Window & window) override
-    {
-        WindowEventHandler::initialize(window);
-
-        glClearColor(0.2f, 0.3f, 0.4f, 1.f);
-
-        m_cornerBuffer = new Buffer();
-		m_program = new Program();
-		m_vao = new VertexArray();
-
-		m_program->attach(
-            Shader::fromString(GL_VERTEX_SHADER,  vertexShaderCode),
-            Shader::fromString(GL_FRAGMENT_SHADER, fragmentShaderCode));
-
-        m_cornerBuffer->setData(std::array<vec2, 4>{ {
-			vec2(0, 0), vec2(1, 0), vec2(0, 1), vec2(1, 1) } }, GL_STATIC_DRAW);
-
-        m_vao->binding(0)->setAttribute(0);
-		m_vao->binding(0)->setBuffer(m_cornerBuffer, 0, sizeof(vec2));
-        m_vao->binding(0)->setFormat(2, GL_FLOAT);
-        m_vao->enable(0);
-    }
-    
-    virtual void paintEvent(PaintEvent & event) override
-    {
-        WindowEventHandler::paintEvent(event);
-
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        m_program->use();
-        m_vao->drawArrays(GL_TRIANGLE_STRIP, 0, 4);
-    }
-
-private:
-    ref_ptr<VertexArray> m_vao;
-    ref_ptr<Buffer> m_cornerBuffer;
-    ref_ptr<Program> m_program;
-};
+    program->use();
+    vao->drawArrays(GL_TRIANGLE_STRIP, 0, 4);
+}
 
 
 int main(int /*argc*/, char * /*argv*/[])
 {
-    info() << "Usage:";
-    info() << "\t" << "ESC" << "\t\t"       << "Close example";
-    info() << "\t" << "ALT + Enter" << "\t" << "Toggle fullscreen";
-    info() << "\t" << "F11" << "\t\t"       << "Toggle fullscreen";
-    info() << "\t" << "F10" << "\t\t"       << "Toggle vertical sync";
+    // Initialize GLFW with error callback and needed OpenGL version window hint
+    glfwInit();
+    glfwSetErrorCallback( [] (int /*error*/, const char * description) { puts(description); } );
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
 
-    ContextFormat format;
-    format.setVersion(3, 1);
-    format.setForwardCompatible(true);
+    // Create a context and, if valid, make it current
+    GLFWwindow * window = glfwCreateWindow(1024, 768, "", NULL, NULL);
+    if (window == nullptr)
+    {
+        critical() << "Context creation failed. Terminate execution.";
 
-    Window::init();
-
-    Window window;
-    window.setEventHandler(new EventHandler());
-
-    if (!window.create(format, "Wiki Example"))
+        glfwTerminate();
         return 1;
+    }
+    glfwMakeContextCurrent(window);
 
-    window.show();
+    // Create callback that when user presses ESC, the context should be destroyed and window closed
+    glfwSetKeyCallback(window, key_callback);
 
-    return MainLoop::run();
+    // Initialize globjects (internally initializes glbinding, and registers the current context)
+    globjects::init();
+
+    // Dump information about context and graphics card
+    info() << std::endl
+        << "OpenGL Version:  " << glbinding::ContextInfo::version() << std::endl
+        << "OpenGL Vendor:   " << glbinding::ContextInfo::vendor() << std::endl
+        << "OpenGL Renderer: " << glbinding::ContextInfo::renderer() << std::endl;
+    
+
+    glClearColor(0.2f, 0.3f, 0.4f, 1.f);
+
+    {
+        // Initialize
+        ref_ptr<Buffer> cornerBuffer = new Buffer();
+        ref_ptr<Program> program = new Program();
+        ref_ptr<VertexArray> vao = new VertexArray();
+
+        program->attach(
+            Shader::fromString(GL_VERTEX_SHADER,  vertexShaderCode),
+            Shader::fromString(GL_FRAGMENT_SHADER, fragmentShaderCode));
+
+        cornerBuffer->setData(std::array<vec2, 4>{ {
+            vec2(0, 0), vec2(1, 0), vec2(0, 1), vec2(1, 1) } }, GL_STATIC_DRAW);
+
+        vao->binding(0)->setAttribute(0);
+        vao->binding(0)->setBuffer(cornerBuffer, 0, sizeof(vec2));
+        vao->binding(0)->setFormat(2, GL_FLOAT);
+        vao->enable(0);
+
+
+        // Main loop
+        while (!glfwWindowShouldClose(window))
+        {
+            glfwPollEvents();
+            draw(program, vao);
+            glfwSwapBuffers(window);
+        }
+
+    }
+
+    // Properly shutdown GLFW
+    glfwTerminate();
+
+    return 0;
 }
