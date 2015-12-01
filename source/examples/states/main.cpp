e02fd8ed3b75708030414a30bb53f76807094a8c
Leander Neiß <leander.neiss@student.hpi.uni-potsdam.de>
2015-12-01 12:20:01 +0100

    Add F5 (shader reload) and F11 (fullscreen toggle) for states example. #291

--- source/examples/states/main.cpp
+++ source/examples/states/main.cpp
@@ -9,6 +9,7 @@
 #include <GLFW/glfw3.h>
 
 #include <globjects/globjects.h>
+#include <globjects/base/File.h>
 #include <globjects/logging.h>
 
 #include <globjects/Buffer.h>
@@ -24,10 +25,69 @@ using namespace glm;
 using namespace globjects;
 
 
+namespace {
+    bool toggleFS = false;
+    bool isFS = false;
+}
+
+
 void key_callback(GLFWwindow * window, int key, int /*scancode*/, int action, int /*modes*/)
 {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
         glfwSetWindowShouldClose(window, true);
+
+    if (key == GLFW_KEY_F5 && action == GLFW_RELEASE)
+        File::reloadAll();
+
+    if (key == GLFW_KEY_F11 && action == GLFW_RELEASE)
+        toggleFS = true;
+}
+
+GLFWwindow * initialize(bool fs = false)
+{
+    // Set GLFW window hints
+    glfwSetErrorCallback( [] (int /*error*/, const char * description) { puts(description); } );
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
+
+    // Create a context and, if valid, make it current
+    GLFWwindow * window = glfwCreateWindow(1024, 768, "", fs ? glfwGetPrimaryMonitor() : NULL, NULL);
+    if (window == nullptr)
+    {
+        critical() << "Context creation failed. Terminate execution.";
+
+        glfwTerminate();
+        exit(1);
+    }
+    glfwMakeContextCurrent(window);
+
+    // Create callback that when user presses ESC, the context should be destroyed and window closed
+    glfwSetKeyCallback(window, key_callback);
+
+    // Initialize globjects (internally initializes glbinding, and registers the current context)
+    globjects::init();
+
+    // Do only on startup
+    if (!toggleFS)
+    {
+       // Dump information about context and graphics card
+       info() << std::endl
+           << "OpenGL Version:  " << glbinding::ContextInfo::version() << std::endl
+           << "OpenGL Vendor:   " << glbinding::ContextInfo::vendor() << std::endl
+           << "OpenGL Renderer: " << glbinding::ContextInfo::renderer() << std::endl;
+    }   
+
+    glClearColor(0.2f, 0.3f, 0.4f, 1.f);
+
+    isFS = fs;
+    return window;
+}
+
+void deinitialize(GLFWwindow * window)
+{
+    globjects::detachAllObjects();
+    glfwDestroyWindow(window);
 }
 
 void draw(Program * shaderProgram, VertexArray * vao, State * thinnestPointSizeState, State * thinPointSizeState, State * normalPointSizeState, State * thickPointSizeState, State * disableRasterizerState, State * enableRasterizerState, State * defaultPointSizeState)
@@ -78,41 +138,21 @@ void draw(Program * shaderProgram, VertexArray * vao, State * thinnestPointSizeS
 
 int main(int /*argc*/, char * /*argv*/[])
 {
-    // Initialize GLFW with error callback and window hints
+    // Initialize GLFW
     glfwInit();
-    glfwSetErrorCallback( [] (int /*error*/, const char * description) { puts(description); } );
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
 
-    // Create a context and, if valid, make it current
-    GLFWwindow * window = glfwCreateWindow(1024, 768, "", NULL, NULL);
-    if (window == nullptr)
+    GLFWwindow * window = nullptr;
+
+    do
     {
-        critical() << "Context creation failed. Terminate execution.";
+        // Deinitialize old window before fullscreen toggle
+        if (window != nullptr) deinitialize(window);
 
-        glfwTerminate();
-        return 1;
-    }
-    glfwMakeContextCurrent(window);
+        // Initialize window
+        window = initialize(toggleFS ? !isFS : isFS);
+        toggleFS = false;
 
-    // Create callback that when user presses ESC, the context should be destroyed and window closed
-    glfwSetKeyCallback(window, key_callback);
-
-    // Initialize globjects (internally initializes glbinding, and registers the current context)
-    globjects::init();
-
-    // Dump information about context and graphics card
-    info() << std::endl
-        << "OpenGL Version:  " << glbinding::ContextInfo::version() << std::endl
-        << "OpenGL Vendor:   " << glbinding::ContextInfo::vendor() << std::endl
-        << "OpenGL Renderer: " << glbinding::ContextInfo::renderer() << std::endl;
-    
-
-    glClearColor(0.2f, 0.3f, 0.4f, 1.f);
-
-    {
-        // Initialize
+        // Initialize OpenGL objects
         ref_ptr<State> defaultPointSizeState  = new State();
         defaultPointSizeState->pointSize(getFloat(GL_POINT_SIZE));
         ref_ptr<State> thinnestPointSizeState = new State();
@@ -155,14 +195,17 @@ int main(int /*argc*/, char * /*argv*/[])
 
 
         // Main loop
-        while (!glfwWindowShouldClose(window))
+        while (!toggleFS && !glfwWindowShouldClose(window))
         {
             glfwPollEvents();
+
             draw(shaderProgram, vao, thinnestPointSizeState, thinPointSizeState, normalPointSizeState, thickPointSizeState, disableRasterizerState, enableRasterizerState, defaultPointSizeState);
+
             glfwSwapBuffers(window);
         }
-
+        
     }
+    while (!glfwWindowShouldClose(window));
 
     // Properly shutdown GLFW
     glfwTerminate();
