8deecec5139abbc9efa7a2548b800462a809395c
Willy Scheibel <willy.scheibel@cginternals.com>
2016-05-26 22:21:55 +0200

    Documentation for AbstractFunctionCall

--- source/globjects/include/globjects/base/AbstractFunctionCall.h
+++ source/globjects/include/globjects/base/AbstractFunctionCall.h
@@ -7,15 +7,47 @@
 namespace globjects
 {
 
-
+/**
+* @brief
+*   The interface for a callable function with variadic argument types.
+*
+* @see
+*   FunctionCall
+*/
 class GLOBJECTS_API AbstractFunctionCall
 {
 public:
-	AbstractFunctionCall();
-	virtual ~AbstractFunctionCall();
+    /**
+    *  @brief
+    *    Constructor
+    */
+    AbstractFunctionCall();
 
-	virtual void operator()() = 0;
-	virtual void * identifier() const = 0;
+    /**
+    * @brief
+    *    Virtual destructor
+    *
+    * Required for correct cleanup of subclasses
+    */
+    virtual ~AbstractFunctionCall();
+
+    /**
+    * @brief
+    *   Entry point for the main functionality of a subclass
+    *
+    * As this interface represents some kind of method object,
+    * a subclass defines this operator to implement its full behavior.
+    */
+    virtual void operator()() = 0;
+
+    /**
+    * @brief
+    *   Accessor for an arbitrary identifier that can be used as identity
+    *
+    * @return
+    *   An identifier for identity tracking
+    */
+    virtual void * identifier() const = 0;
 };
 
 
