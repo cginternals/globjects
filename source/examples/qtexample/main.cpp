1356ab539cfe9a5d490a0729212ad8e699862127
Willy Scheibel <willyscheibel@gmx.de>
2017-03-22 21:21:17 +0100

    Use updateGL instead of direct redraw

--- source/examples/qtexample/main.cpp
+++ source/examples/qtexample/main.cpp
@@ -122,20 +122,17 @@ public:
     {
         makeCurrent();
 
-        bool reloadedShaders = false;
         switch (event->key())
         {
         case Qt::Key_F5:
             m_vertexShaderSource->reload();
             m_fragmentShaderSource->reload();
-            reloadedShaders = true;
+            updateGL();
             break;
         default:
             break;
         }
         doneCurrent();
-        if(reloadedShaders)
-            paint();
     }
 
 
